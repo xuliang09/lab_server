0e7f05cc77053f73c0fb22f1f58434d5b13c963b
Xul <294739212@qq.com>
2019-11-17 19:39:30 +0800

    Update search_ip.py

--- search_ip.py
+++ search_ip.py
@@ -1,20 +1,26 @@
 #-*- coding: utf-8 -*-
 import paramiko
 import json
-from  multiprocessing.pool import ThreadPool
+import sys
+import multiprocessing
 
 
 def search_ip(ip, port, username, password, command):
-    ssh = paramiko.SSHClient()
-    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-    ssh.connect(ip, port, username, password, timeout=1)
-    print('@'*30, ip, '@'*30, end='\n\n')
-    for cmd in command:
-        stdin, stdout, stderr = ssh.exec_command(cmd)
-        for line in stdout.readlines():
-            print(line.strip())
-    print('@'*90, end='\n\n\n')
-    ssh.close()
+    try:
+        ssh = paramiko.SSHClient()
+        ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        ssh.connect(ip, port, username, password, timeout=1)
+        print('@'*30, ip, '@'*30, end='\n\n')
+        for cmd in command:
+            stdin, stdout, stderr = ssh.exec_command(cmd)
+            for line in stdout.readlines():
+                print(line.strip())
+        print('@'*90, end='\n\n\n')
+    except Exception as e:
+        # print(ip, e)
+        pass
+    finally:
+        ssh.close()
 
 
 if __name__=='__main__':
@@ -24,9 +30,9 @@ if __name__=='__main__':
     username = config['username']
     password = config['password']
     port = config['port']
-    thread_num = config['thread_num']
+    process_num = config['process_num']
     ipv4_mask = config['ipv4_mask']
-    thread_pool = ThreadPool(thread_num)
+    pool = multiprocessing.Pool(process_num)
     
     ipv4_mask_list = []
     for item in ipv4_mask.strip().split('.'):
@@ -38,7 +44,7 @@ if __name__=='__main__':
             for j in range(ipv4_mask_list[2][0], ipv4_mask_list[2][1]):
                 for i in range(ipv4_mask_list[3][0], ipv4_mask_list[3][1]):
                     ip = '{}.{}.{}.{}'.format(m, k, j, i)
-                    thread_pool.apply_async(search_ip, args=(ip, port, username, password, command))
+                    pool.apply_async(search_ip, args=(ip, port, username, password, command))
 
-    thread_pool.close()
-    thread_pool.join()
+    pool.close()
+    pool.join()
